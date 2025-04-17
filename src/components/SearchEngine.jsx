a324e0fcbef0588b51fcaa340d634efea984d2ef
Mann Merwana <mann.merwana27@gmail.com>
2025-04-17 17:13:30 +0530

    Cached JSON Data to Reduce API Calls

--- src/components/SearchEngine.jsx
+++ src/components/SearchEngine.jsx
@@ -5,8 +5,16 @@ import { useEffect,useState } from 'react';
    const [searchInput, setSearchInput] = useState("");
    const [results, setResults] = useState([]);
    const [showResults, setShowResults] = useState(false);
+   const [cache, setCache] = useState({}); //state to implement and handle cache to cache api calls
 
    const fetchData = async () => {
+     //if data is present in cache,setResults to the input
+     if (cache[searchInput]) {
+       console.log(`CACHE RETURNED ${searchInput}`);
+       setResults(cache[searchInput]);
+       return;
+     }
+     //if data is not present in the cache,then only make an api call
      const timeTakenForEachApiCallInMs = searchInput;
      console.log(`API Call ${timeTakenForEachApiCallInMs}`);
      const resData = await fetch(
@@ -15,6 +23,8 @@ import { useEffect,useState } from 'react';
 
      const jsonData = await resData.json();
      setResults(jsonData?.recipes);
+     //                            here key is Mango as ex and jsondata is the result that we have got.
+     setCache((prev) => ({ ...prev, [searchInput]: jsonData?.recipes }));
    };
    //debouncing to reduce api calls.
    useEffect(() => {
@@ -26,7 +36,7 @@ import { useEffect,useState } from 'react';
 
    return (
      <>
-      
+       <h1>Auto Complete Search Bar</h1>
        <div>
          <input
            type="text"
